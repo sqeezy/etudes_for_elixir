da2d15cbbd0d781f5e9bfff32473cab796669150
Anton Herzog <info@antonherzog.com>
2017-07-06 23:16:55 +0200

    Implement raise.

---
+++ lib/powers.ex
@@ -0,0 +1,14 @@
+defmodule Powers do
+  import Kernel, except: [raise: 2]
+
+  def raise(_, 0), do: 1
+  def raise(x, 1), do: x
+
+  def raise(x, n) when n > 0 do
+    x * raise(x, n - 1)
+  end
+
+  def raise(x, n) when n < 0 do
+    1 / raise(x, -n)
+  end
+end
---
+++ test/powers_test.exs
@@ -0,0 +1,11 @@
+defmodule PowersTest do
+  use ExUnit.Case
+
+  test "raise works" do
+    assert Powers.raise(2, 2) == 4
+    assert Powers.raise(2, 3) == 8
+    assert Powers.raise(10, 2) == 100
+    assert Powers.raise(12345, 0) == 1
+    assert Powers.raise(12345, 1) == 12345
+  end
+end
