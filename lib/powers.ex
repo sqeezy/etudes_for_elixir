4d49de677cdd2db7f11e5549e163e7760c6a8b79
Anton Herzog <info@antonherzog.com>
2017-07-06 23:41:12 +0200

    Refactoring.

--- lib/powers.ex
+++ lib/powers.ex
@@ -3,20 +3,9 @@ defmodule Powers do
 
   def raise(_, 0), do: 1
   def raise(x, 1), do: x
+  def raise(x, n) when n > 0, do: raise(x, n, 1)
+  def raise(x, n) when n < 0, do: 1 / raise(x, -n)
+  defp raise(_, 0, acc), do: acc
+  defp raise(x, n, acc), do: raise(x, n - 1, x * acc)
 
-  def raise(x, n) when n > 0 do
-    raise(x, n, 1)
-  end
-
-  def raise(x, n) when n < 0 do
-    1 / raise(x, -n)
-  end
-
-  defp raise(_, 0, acc) do
-    acc
-  end
-
-  defp raise(x, n, acc) do
-    raise(x, n - 1, x * acc)
-  end
 end
